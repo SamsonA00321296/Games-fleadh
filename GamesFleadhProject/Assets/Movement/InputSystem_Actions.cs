674783d9fb0c2f51614bcb11640f29d03437d20b
SamsonA00321296 <samson.glover2014@gmail.com>
2025-03-01 18:00:02 +0000

    Shift now boosts
    sorry for flooding the repository lmao

--- GamesFleadhProject/Assets/Movement/InputSystem_Actions.cs
+++ GamesFleadhProject/Assets/Movement/InputSystem_Actions.cs
@@ -465,7 +465,7 @@ public partial class @PlayerInputActions: IInputActionCollection2, IDisposable
                 {
                     ""name"": """",
                     ""id"": ""1c04ea5f-b012-41d1-a6f7-02e963b52893"",
-                    ""path"": ""<Keyboard>/e"",
+                    ""path"": ""<Keyboard>/leftShift"",
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": ""Keyboard&Mouse"",
