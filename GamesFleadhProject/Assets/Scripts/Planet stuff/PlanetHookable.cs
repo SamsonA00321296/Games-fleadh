1b1ae6a13fda20f9f8ec9cc1957b2ae4a5e4f483
Sam <sammcaulay2211@gmail.com>
2025-03-03 17:01:47 +0000

    fix to rotating, tag and hooking moons
    this should hopefully be the fix that shall make all the planets hookable, all the planets keep thier tags, and will rotate around the planet, if this breaks again, im going to TWEAK

--- GamesFleadhProject/Assets/Scripts/Planet stuff/PlanetHookable.cs
+++ GamesFleadhProject/Assets/Scripts/Planet stuff/PlanetHookable.cs
@@ -5,11 +5,29 @@ namespace Planet_stuff
     public class MoonFixedJointAttacher : MonoBehaviour
     {
         private FixedJoint2D _fixedJoint;
+        
+        public float rotationalSpeed;
+        public GameObject sunPivotObject;
+
+        private bool _isHooked;
+        
 
         void Start()
         {
             // Cache the FixedJoint2D component attached to this moon prefab.
             _fixedJoint = GetComponent<FixedJoint2D>();
+            rotationalSpeed = Random.Range(-15.0f, -5.0f);
+        }
+       
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (!_isHooked)
+            {
+                transform.RotateAround(sunPivotObject.transform.position, Vector3.forward, rotationalSpeed * Time.deltaTime);
+            }
+
         }
 
         void OnTriggerEnter2D(Collider2D other)
@@ -17,6 +35,7 @@ namespace Planet_stuff
             // Check if the other object has the "Hook" tag.
             if (other.CompareTag("Hook"))
             {
+                _isHooked = true;
                 // Get the Rigidbody2D of the object that entered the trigger.
                 Rigidbody2D otherBody = other.GetComponent<Rigidbody2D>();
                 InitialSpikeThrust initialSpikeThrust = other.GetComponent<InitialSpikeThrust>();
